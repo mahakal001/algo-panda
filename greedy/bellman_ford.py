58d135e052b4480980ed0864af57dcd6d4c315d1
Mohit Pilkhan <mohitpilkhan@gofynd.com>
2020-11-15 21:24:39 +0530

    Added bellman ford

---
+++ greedy/bellman_ford.py
@@ -0,0 +1,40 @@
+def relax(u, v, w, d_arr):
+    if d_arr[u] + w < d_arr[v] :
+        d_arr[v] = d_arr[u] + w
+
+def bellman_ford(weighted_edges, start_node, n):
+    """
+
+    :param edges: A list of edges where each edge is represented by pairs of nodes that produce it
+    """
+
+    adj_array = [[] for _ in range(n + 1)]  # because of 1 based indexing
+    d_arr = [ 9999 for _ in range(n + 1) ]
+    d_arr[start_node] = 0
+
+    for edge in weighted_edges:
+        adj_array[edge[0]].append([edge[1], edge[2]])
+
+    for i in range(1,n):    # run n-1 time
+        for u,v,w in weighted_edges:
+            relax(u, v ,w, d_arr)
+
+    for u,v,w in weighted_edges:
+        if d_arr[v] > d_arr[u] + w:
+            return False, None
+
+    return True, d_arr
+
+weighted_edges = [[1,2,10], [1,5,5], [2,3,1], [2,5,2], [3,4,4], [4,3,6], [4,1,7], [5,4,2], [5,3,9], [5,1,3] ]
+total_nodes = 5
+path, distances = bellman_ford(weighted_edges, 1, total_nodes)
+
+print("path: ", path)
+print("Distances: ", distances[1:])
+
+
+
+
+
+
+
--- greedy/dijkstra_sssp.py
+++ greedy/dijkstra_sssp.py
@@ -11,11 +11,8 @@ def dijkstra(weighted_edges, start_node, n):
     adj_array = [[] for _ in range(n + 1)]  # because of 1 based indexing
     d_arr = [ 9999 for _ in range(n + 1) ]
 
-
     for edge in weighted_edges:
         adj_array[edge[0]].append([edge[1], edge[2]])
-
-    print(adj_array)
 
     A = []
     A.append(start_node)
@@ -39,19 +36,16 @@ def dijkstra(weighted_edges, start_node, n):
 
         A.append(min_index)
         cur_node = min_index
-        print(cur_node)
         unexplored_set.remove(cur_node)
-        print(A)
 
-    print(A)
     return A, d_arr
 
 weighted_edges = [[1,2,10], [1,5,5], [2,3,1], [2,5,2], [3,4,4], [4,3,6], [4,1,7], [5,4,2], [5,3,9], [5,1,3] ]
 total_nodes = 5
 path, distances = dijkstra(weighted_edges, 1, total_nodes)
 
-print("path: ", path)
-print("Distances: ", distances[1:])
+print("The order in which the edges are added : ", path)
+print("distances from start node : ", distances[1:])
 
 
 
